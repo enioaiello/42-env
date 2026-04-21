41acbc440903a55b72822256ca29f6fc9c0d978f
Enio Aiello <enio@corundum.fr>
2026-04-21 20:09:03 +0200

    feat: add required display functions
    Add display_error, display_success, display_warning and display_info functions after the insert_new_line function

--- script/setup-my-env.sh
+++ script/setup-my-env.sh
@@ -4,3 +4,25 @@
 function insert_new_line() {
 	echo ""
 }
+
+# Affiche une erreur
+function display_error() {
+	echo "Error: $1" >&2
+	echo "The script will now close."
+	exit 1 # Sort avec le code d'erreur 1
+}
+
+# Affiche un succès
+function display_success() {
+	echo "Success: $1"
+}
+
+# Affiche un avertissement
+function display_warning() {
+	echo "Warning: $1"
+}
+
+# Affiche une information
+function display_info() {
+	echo "Info: $1"
+}
